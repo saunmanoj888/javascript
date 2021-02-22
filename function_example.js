f40226143309819081111f092fb869514cd5f3fd
saunmanoj888 <manoj@kiprosh.com>
2021-02-22 17:54:11 +0530

    implement function example

---
+++ function_example.js
@@ -0,0 +1,13 @@
+function calculateAge(dateString){
+    let today = new Date();
+    let birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    return age;
+}
+
+console.log(calculateAge("1995-09-06"))
