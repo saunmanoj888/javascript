fd576133f3b4fef228332385676f452168e34e29
saunmanoj888 <manoj@kiprosh.com>
2021-02-22 18:49:00 +0530

    implement function as exepression

--- function_example.js
+++ function_example.js
@@ -11,3 +11,19 @@ function calculateAge(dateString){
 }
 
 console.log(calculateAge("1995-09-06"))
+
+// create function using function expression
+
+let getAge  = function(dateString){
+    let today = new Date();
+    let birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    return age;
+};
+
+console.log(getAge("1995-09-06"))
